a8dc228128c76e2d29eec31c228977d35990a802
sethrhod <iamsethrhodes@gmail.com>
2023-02-02 16:57:04 -0500

    added speaker list to speaker page

--- components/Speakers.js
+++ components/Speakers.js
@@ -1,28 +1,49 @@
-import { StyleSheet, SafeAreaView, FlatList, StatusBar, View, Text } from "react-native";
+import { StyleSheet, SafeAreaView, FlatList, StatusBar, View, Text, Image, Linking } from "react-native";
+import React, { useState, useEffect } from 'react';
 import { LinearGradient } from "expo-linear-gradient";
+import { FontAwesome5 } from "@expo/vector-icons";
 
-const DATA = [
-    {
-      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-      title: 'First Item',
-    },
-    {
-      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-      title: 'Second Item',
-    },
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: 'Third Item',
-    },
-  ];
+const Item = (props) => (
+  <View style={styles.item}>
 
-const Item = ({title}) => (
-<View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-</View>
+    {/*profile pic*/}
+
+    <Image style={styles.logo} source={{uri: props.uri}}/>
+
+    {/*Name and links*/}
+
+    <View style={{ alignItems: 'center' }}>
+      <Text style={styles.title}>{props.title}</Text>
+      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
+        {props.links.map((link) => 
+          {var title = link.title;
+            return (
+              <View style={{ justifyContent: 'center', padding: 5 }}>
+                <FontAwesome5 name={title.toLowerCase()} size={17} color="#0099CC" item_container/>
+              </View>
+          )}
+        )}
+      </View>
+    </View>
+
+    {/*bio*/}
+
+    <View style={{ width: 120 }}>
+      <Text style={styles.bio}>{props.bio}</Text>
+    </View>
+  </View>
 );
 
 export default function Speakers() {
+
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch("https://sessionize.com/api/v2/jl4ktls0/view/Speakers")
+    .then(response => response.json())
+    .then(data => setData(data))
+  }, [])
+
   return (
     <LinearGradient
       Background
@@ -33,9 +54,11 @@ export default function Speakers() {
     >
       <SafeAreaView style={styles.item_container}>
         <FlatList
-          data={DATA}
-          renderItem={({ item }) => <Item title={item.title} />}
+          data={data}
+          renderItem={({ item }) => <Item title={item.fullName} uri={item.profilePicture} bio={item.bio} links={item.links} />}
           keyExtractor={(item) => item.id}
+          contentContainerStyle={{alignItems: 'stretch'}}
+          style={{ width: '100%' }}
         />
       </SafeAreaView>
     </LinearGradient>
@@ -50,15 +73,33 @@ const styles = StyleSheet.create({
   },
   item_container: {
     flex: 1,
+    width: '100%',
     marginTop: StatusBar.currentHeight || 0,
   },
   item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    borderRadius: 15,
+    width: '90%',
+    backgroundColor: '#FFFFFF',
+    padding: 10,
     marginVertical: 8,
     marginHorizontal: 16,
   },
   title: {
-    fontSize: 32,
+    fontSize: 20,
+    alignSelf: 'flex-start'
   },
+  bio: {
+    fontSize: 12,
+  },
+  logo: {
+    width: 70,
+    height: 70,
+    borderRadius: 40,
+  },
+  link: {
+    color: '#0099CC'
+  }
 });
+
