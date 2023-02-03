b1354bb78eb94f62f77399738f95f65ed990f95d
Spadeful <iamsethrhodes@gmail.com>
2023-02-03 14:49:47 -0500

    Created list for sponsors and started on schedule

--- components/Schedule.js
+++ components/Schedule.js
@@ -1,24 +1,70 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
+function Timeblock(props) {
+  return (
+    <View style={styles.timeblock}>
+      <Text style={styles.timeblock_text}>{props.time}</Text>
+    </View>
+  );
+}
+
 export default function Schedule() {
-    return (
-        <LinearGradient
-        Background
-        Linear
-        Gradient
-        colors={["rgba(0,0,0,1)", "rgba(0,47,63,1)"]}
-        style={styles.container}
-        >       
-            <Text>Schedule</Text>
-        </LinearGradient>
-    );
+  const times = [
+    "7:45 AM",
+    "8:45 AM",
+    "10:00 AM",
+    "11:15 AM",
+    "12:15 PM",
+    "1:15 PM",
+    "2:30 PM",
+    "3:45 PM",
+    "5:00 PM",
+  ];
+
+  const timesarray = times.map((time) => {
+    return <Timeblock time={time} />;
+  });
+
+  return (
+    <LinearGradient
+      Background
+      Linear
+      Gradient
+      colors={["rgba(0,0,0,1)", "rgba(0,47,63,1)"]}
+      style={styles.container}
+    >
+      <FlatList
+        data={times}
+        renderItem={({ time }) => <Timeblock time={time} />}
+        horizontal={true}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{ alignItems: "stretch" }}
+        style={{ width: "100%" }}
+      />
+    </LinearGradient>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  timeblock_text: {
+    color: "white",
+    fontSize: 20,
+  },
+  timesarray: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  timeblock: {
+    backgroundColor: "rgba(255,255,255,0.1)",
+    width: 50,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
--- components/Sponsors.js
+++ components/Sponsors.js
@@ -1,24 +1,104 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  FlatList,
+  Image,
+  StatusBar,
+  Text,
+} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
+const SubItem = (props) => (
+  <View style={styles.logo_container}>
+    <Image style={styles.logo} source={{ uri: props.sponsors.uri }} />
+  </View>
+);
+
+const Item = (props) => (
+  <View style={styles.sponsor_level_container}>
+    <Text style={styles.sponsor_level}>{props.item.sponsor_level}</Text>
+    <FlatList
+      data={props.item.sponsors}
+      renderItem={({ item }) => <SubItem sponsors={item} />}
+      contentContainerStyle={{ alignItems: "stretch", }}
+      style={{ width: "100%" }}
+    />
+  </View>
+);
+
+function List() {
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("https://atlcloudconf.com/sponsors.json")
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => console.log("done"));
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.item_container}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <Item item={item} />}
+        contentContainerStyle={{ alignItems: "stretch" }}
+        style={{ width: "100%" }}
+      />
+    </SafeAreaView>
+  )
+}
+
 export default function Sponsors() {
-    return (
-        <LinearGradient
-        Background
-        Linear
-        Gradient
-        colors={["rgba(0,0,0,1)", "rgba(0,47,63,1)"]}
-        style={styles.container}
-        >       
-            <Text>Sponsors</Text>
-        </LinearGradient>
-    );
+  return (
+    <LinearGradient
+      Background
+      Linear
+      Gradient
+      colors={["rgba(0,0,0,1)", "rgba(0,47,63,1)"]}
+      style={styles.container}
+    >
+      <List />
+    </LinearGradient>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  item_container: {
+    flex: 1,
+    width: "100%",
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  sponsor_level_container : {
+    alignItems: "center",
+    width: "90%",
+    padding: 5,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  sponsor_level: {
+    fontSize: 32,
+    color: "white",
+  },
+  logo_container: {
+    backgroundColor: "#FFFFFF",
+    alignItems: "center",
+    borderRadius: 15,
+    width: "100%",
+    padding: 5,
+    marginVertical: 8,
+  },
+  logo: {
+    width: '100%',
+    height: 100,
+    resizeMode: "contain",
+  },
 });
